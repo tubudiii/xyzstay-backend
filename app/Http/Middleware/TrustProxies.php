faeee2d1b898605dc96d442e9c7c2433af5f3d98
tubudiii <budisubawa123@gmail.com>
2025-09-25 19:16:30 +0700

    feat(TrustProxies.php): add TrustProxies middleware for handling trusted proxies

---
+++ app/Http/Middleware/TrustProxies.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
+
+class TrustProxies extends Middleware
+{
+    /**
+     * The trusted proxies for this application.
+     * Use '*' to trust all proxies (useful in containerized / cloud environments).
+     *
+     * @var array|string|null
+     */
+    protected $proxies = '*';
+
+    /**
+     * The headers that should be used to detect proxies.
+     *
+     * @var int
+     */
+    protected $headers = Request::HEADER_X_FORWARDED_ALL;
+}
